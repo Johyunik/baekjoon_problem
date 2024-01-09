7103a4403443d2dd052e312a7c254feb5387f0d2
Johyunik <whgusdlr13@naver.com>
2024-01-09 18:33:50 +0900

    2024-01-09

---
+++ src/main/java/com/example/baekjoon/calculation/Calculator_solve.java
@@ -0,0 +1,17 @@
+package com.example.baekjoon.calculation;
+
+import java.util.Scanner;
+
+public class Calculator_solve {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        long A = scanner.nextLong();
+        long B = scanner.nextLong();
+        long C = scanner.nextLong();
+
+        System.out.println(A + B + C);
+
+    }
+}
